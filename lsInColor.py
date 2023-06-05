b55499840f01d64fd5f2e2c79f3e5ede0e12241b
taylfw <frank.w.taylor88@gmail.com>
2023-06-05 08:05:58 -0400

    first commit

---
+++ lsInColor.py
@@ -0,0 +1,33 @@
+import subprocess
+
+# print(colored('hello', 'red'), colored('world', 'green'))
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+contents = subprocess.Popen(["ls -l"], stdout=subprocess.PIPE, shell=True)
+(out, err) = contents.communicate()
+
+out = str(out)
+out = out.strip("b'total 4")
+out = out.split("\\")
+
+for i in out:
+    i = i.lstrip('n')
+    print(
+          bcolors.OKCYAN +i[0:10],
+          bcolors.OKGREEN + i[11:26],
+          bcolors.WARNING + i[27:30],
+          bcolors.FAIL + i[31:43], 
+          bcolors.OKBLUE + i[44:]
+          )
+
+
